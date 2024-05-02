572854c225d3eb83503f8c3d0cbbe2a154df9778
Yaren Yalçınkaya <115217733+yarenyalcinkaya@users.noreply.github.com>
2024-05-02 13:00:43 +0300

    Add files via upload

---
+++ src/main/java/com/odev/odev/Controller.java
@@ -0,0 +1,14 @@
+package com.odev.odev;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Controller {
+    @GetMapping("/getProjeBilgileri")
+    public String getProjeBilgileri(){
+        return "Odev biligilerim";
+    }
+
+}
---
+++ src/test/java/com/odev/odev/OdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odev.odev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
